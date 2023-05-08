f8aa71c7969af69584237118a0e4631924b55843
sentey <91223447+fraanab@users.noreply.github.com>
2023-05-07 21:29:33 -0300

    Update settings.py
    templates dir

--- backend/settings.py
+++ backend/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['core/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
